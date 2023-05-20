fa68aedbc1fb95662570877413b1d96d408974f2
Samuel <victorsamuelpraise@gmail.com>
2023-05-20 18:55:21 +0000

    handled tokenization of input

--- read_line.c
+++ read_line.c
@@ -8,12 +8,11 @@ char *read_line(void)
 	int line = 0;
 	char *buffer = NULL;
 	size_t size = 0;
-	line = getline(&buffer, &size, stdin);
 
+	line = getline(&buffer, &size, stdin);
 	if (line == EOF)
 	{
 		free(buffer);
-
 		if (isatty(STDIN_FILENO))
 		{
 			write(STDOUT_FILENO, "\n", 1);
--- shell.h
+++ shell.h
@@ -11,17 +11,24 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <sys/types.h>
-
+#define DELIM " ,!¡¿?\'\"\n"
 
 /* print functions */
 int _putchar(char c);
 int print_str(char *str);
+
+/*handle string */
 int _strlen(char *str);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _strncmp(char *s1, char *s2, int n);
+
+/*handle errors*/
 void handle_error(char *cmd_line); 
+
+/*Tokenizing a line*/
+char **lexical_analysis(char *input);
 
 void *_calloc(unsigned int num, unsigned int size);
 
