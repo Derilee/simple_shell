33608fc65d4ff76238f9b3f78ffeaa9a0b990e41
Samuel <victorsamuelpraise@gmail.com>
2023-05-24 22:20:37 +0000

    fixed betty error

--- _realloc.c
+++ _realloc.c
@@ -58,11 +58,12 @@ int _getline(char **lineptr, int fd)
 			end = 0;
 	}
 	if (lineptr == NULL)
-		return 0;
+		return (0);
 	if (*lineptr == NULL)
 	{
 		*lineptr = malloc(sizeof(char) * size + 1);
-		if (*lineptr == NULL) return -1;
+		if (*lineptr == NULL)
+			return (-1);
 	}
 	while (1)
 	{
@@ -75,39 +76,47 @@ int _getline(char **lineptr, int fd)
 				sum += r;
 				end = sum;
 				for (d = 0; r != 0 && d < end; d++)
-					if (buffer[d] == '\n') r = 0;
+					if (buffer[d] == '\n')
+						r = 0;
 			}
 			if (sum == 0)
-			{ buffer[0] = 0;
-				return sum;
+			{
+				buffer[0] = 0;
+				return (sum);
 			}
 			buffer[sum] = 0;
 			sum = 0;
 		}
 		for (; buffer[begin]; begin++)
 		{
-			if (begin == 1024) break;
+			if (begin == 1024)
+				break;
 			if (buffer[begin] == '\n')
 			{
 				(*lineptr)[c] = '\n';
 				begin++;
 				c++;
 				(*lineptr)[c] = '\0';
-				return c;
+				return (c);
 			}
-			else (*lineptr)[c] = buffer[begin];
-			c++;
+			else
+			{
+				(*lineptr)[c] = buffer[begin];
+				c++;
+			}
 		}
 		if (c + begin >= 1024)
 		{
 			old_size = size;
 			size = size + 1024;
 			*lineptr = _realloc(*lineptr, old_size, size);
-			if (*lineptr == NULL) return -1;
-		} else
+			if (*lineptr == NULL)
+				return (-1);
+		}
+		else
 		{
 			(*lineptr)[old_size + begin] = 0;
-			return c;
+			return (c);
 		}
 	}
 }
